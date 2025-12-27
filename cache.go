7e0f6d0d56c488eabb7d5a69d7a790d62ffa3e29
Boris <glazunov2142@icloud.com>
2025-12-27 11:07:23 +0400

    Update cache.go

--- cache.go
+++ cache.go
@@ -3,53 +3,42 @@ package ashcache
 import (
 	"context"
 	"github.com/Borislavv/go-ash-cache/internal/cache"
-	"github.com/Borislavv/go-ash-cache/internal/cache/db/model"
 	"github.com/Borislavv/go-ash-cache/internal/config"
 	"github.com/Borislavv/go-ash-cache/internal/evictor"
 	"github.com/Borislavv/go-ash-cache/internal/lifetimer"
 	"github.com/Borislavv/go-ash-cache/internal/shared/cachedtime"
 	"github.com/Borislavv/go-ash-cache/internal/telemetry"
+	"io"
 	"log/slog"
 )
 
 type AshCache interface {
-	Get(key string, callback func(item model.AshItem) ([]byte, error)) (data []byte, err error)
-	Del(key string) (ok bool)
+	cache.Cacher
+	evictor.Evictor
+	lifetimer.Lifetimer
+	telemetry.Logger
+	io.Closer
 }
 
 type Cache struct {
-	cacher    cache.Cacher
-	eviction  evictor.Evictor
-	lifetime  lifetimer.Lifetimer
-	telemeter telemetry.Logger
+	cache.Cacher
+	evictor.Evictor
+	lifetimer.Lifetimer
+	telemetry.Logger
+	cls context.CancelFunc
 }
 
 func New(ctx context.Context, cfg *config.Cache, logger *slog.Logger) *Cache {
+	ctx, cancel := context.WithCancel(ctx)
 	cachedtime.CloseByCtx(ctx)
 	cacher := cache.New(ctx, cfg, logger)
 	eviction := evictor.New(ctx, cfg.Eviction, logger, cacher)
 	lifetime := lifetimer.New(ctx, cfg.Lifetime, logger, cacher)
 	telemeter := telemetry.New(ctx, cfg, logger, cacher, eviction, lifetime, cfg.DB.TelemetryLogsInterval)
-	return &Cache{cacher: cacher, eviction: eviction, lifetime: lifetime, telemeter: telemeter}
+	return &Cache{cls: cancel, Cacher: cacher, Evictor: eviction, Lifetimer: lifetime, Logger: telemeter}
 }
 
-func (c *Cache) Get(key string, callback func(item model.AshItem) ([]byte, error)) (data []byte, err error) {
-	k := model.NewKey(key)
-
-	if entry, ok := c.cacher.Get(k.Value()); ok {
-		if entry.Key().IsTheSame(k) {
-			return entry.PayloadBytes(), nil
-		}
-		// hash collision; rewrite, because it's really rare
-	}
-
-	entry := c.cacher.MakeEntry(k, callback)
-	resp, respErr := callback(entry)
-	if respErr != nil {
-		return nil, respErr
-	}
-	entry.SetPayload(resp)
-	c.cacher.Set(entry)
-
-	return resp, nil
+func (c *Cache) Close() error {
+	c.cls()
+	return nil
 }
