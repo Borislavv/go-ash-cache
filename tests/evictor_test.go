c7ac18d70834afeac1edd94f1224b649d660982e
Boris <glazunov2142@icloud.com>
2025-12-27 12:12:09 +0400

    Update evictor_test.go

--- tests/evictor_test.go
+++ tests/evictor_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"context"
 	"fmt"
 	ashcache "github.com/Borislavv/go-ash-cache"
 	"github.com/Borislavv/go-ash-cache/internal/cache/db/model"
@@ -11,24 +12,84 @@ import (
 	"time"
 )
 
-func TestEvictorEviction(t *testing.T) {
+func TestEvictorListingEviction(t *testing.T) {
 	evictionTestCfg := help.EvictionCfg()
+	evictionTestCfg.Eviction.IsListing = true
 	cache := ashcache.New(t.Context(), evictionTestCfg, help.Logger())
 
 	// attempt to load 10mb in cache
+	const wightKB = 100 * 1024
 	for i := 0; i < 100; i++ {
-		const KB = 1024
 		data, err := cache.Get(fmt.Sprintf("key-%d", i), func(item model.AshItem) ([]byte, error) {
-			data := make([]byte, 100*KB)
+			data := make([]byte, wightKB)
 			return data, nil
 		})
 		require.NoError(t, err)
-		require.Len(t, data, 100*KB)
+		require.Len(t, data, wightKB)
 	}
 
-	time.Sleep(3 * time.Second)
-	length := cache.Len()
-	memory := cache.Mem()
-	require.Equal(t, int64(80), length, fmt.Sprintf("cache length - %d, memory - %s (expected length = 80)", length, bytes.FmtMem(uint64(memory))))
-	require.LessOrEqual(t, evictionTestCfg.Eviction.SoftMemoryLimitBytes, memory, fmt.Sprintf("cache length - %d, memory - %s (expected memory = %s)", length, bytes.FmtMem(uint64(memory)), bytes.FmtMem(uint64(evictionTestCfg.Eviction.SoftMemoryLimitBytes))))
+	ctx, cancel := context.WithTimeout(t.Context(), time.Second*30)
+	defer cancel()
+
+	checkEach := time.NewTicker(time.Millisecond * 100)
+	defer checkEach.Stop()
+
+	expectedMemory := evictionTestCfg.Eviction.SoftMemoryLimitBytes
+	expectedLength := evictionTestCfg.Eviction.SoftMemoryLimitBytes / wightKB
+
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("context deadline exceeded; test failed")
+		case <-checkEach.C:
+			memory := cache.Mem()
+			length := cache.Len()
+			if length <= expectedLength && memory <= expectedMemory {
+				require.LessOrEqual(t, length, expectedLength, fmt.Sprintf("cache length - %d, memory - %s (expected length = 80)", length, bytes.FmtMem(uint64(memory))))
+				require.LessOrEqual(t, memory, expectedMemory, fmt.Sprintf("cache length - %d, memory - %s (expected memory = %s)", length, bytes.FmtMem(uint64(memory)), bytes.FmtMem(uint64(evictionTestCfg.Eviction.SoftMemoryLimitBytes))))
+				return
+			}
+		}
+	}
+}
+
+func TestEvictorSamplingEviction(t *testing.T) {
+	evictionTestCfg := help.EvictionCfg()
+	evictionTestCfg.Eviction.IsListing = false
+	cache := ashcache.New(t.Context(), evictionTestCfg, help.Logger())
+
+	// attempt to load 10mb in cache when threshold is 8mb
+	const wightKB = 100 * 1024
+	for i := 0; i < 100; i++ {
+		data, err := cache.Get(fmt.Sprintf("key-%d", i), func(item model.AshItem) ([]byte, error) {
+			data := make([]byte, wightKB)
+			return data, nil
+		})
+		require.NoError(t, err)
+		require.Len(t, data, wightKB)
+	}
+
+	ctx, cancel := context.WithTimeout(t.Context(), time.Second*30)
+	defer cancel()
+
+	checkEach := time.NewTicker(time.Millisecond * 100)
+	defer checkEach.Stop()
+
+	expectedMemory := evictionTestCfg.Eviction.SoftMemoryLimitBytes
+	expectedLength := evictionTestCfg.Eviction.SoftMemoryLimitBytes / wightKB
+
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatalf("context deadline exceeded; test failed")
+		case <-checkEach.C:
+			memory := cache.Mem()
+			length := cache.Len()
+			if length <= expectedLength && memory <= expectedMemory {
+				require.LessOrEqual(t, length, expectedLength, fmt.Sprintf("cache length - %d, memory - %s (expected length = 80)", length, bytes.FmtMem(uint64(memory))))
+				require.LessOrEqual(t, memory, expectedMemory, fmt.Sprintf("cache length - %d, memory - %s (expected memory = %s)", length, bytes.FmtMem(uint64(memory)), bytes.FmtMem(uint64(evictionTestCfg.Eviction.SoftMemoryLimitBytes))))
+				return
+			}
+		}
+	}
 }
