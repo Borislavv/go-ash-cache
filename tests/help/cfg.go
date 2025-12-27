d22b6e9a0e495a0b851b7d33c4ca7260ed056631
Boris <glazunov2142@icloud.com>
2025-12-27 11:25:57 +0400

    Create cfg.go

---
+++ tests/help/cfg.go
@@ -0,0 +1,47 @@
+package help
+
+import (
+	"github.com/Borislavv/go-ash-cache/internal/config"
+	"time"
+)
+
+func Cfg() *config.Cache {
+	c := &config.Cache{
+		Lifetime: &config.LifetimerCfg{
+			OnTTL:         config.TTLModeRefresh,
+			TTL:           time.Minute * 5,
+			Rate:          1000,
+			Beta:          0.5,
+			Coefficient:   0.5,
+			IsRemoveOnTTL: false,
+		},
+		Eviction: &config.EvictionCfg{
+			LRUMode:              config.LRUModeListing,
+			SoftLimitCoefficient: 0.8,
+			SoftMemoryLimitBytes: 1024 * 1024 * 800,
+			CallsPerSec:          5,
+			BackoffSpinsPerCall:  1024,
+			IsListing:            true,
+		},
+		DB: config.DBCfg{
+			SizeBytes:              1024 * 1024 * 1024,
+			IsTelemetryLogsEnabled: true,
+			TelemetryLogsInterval:  time.Second * 5,
+		},
+	}
+	c.AdjustConfig()
+	return c
+}
+
+func EvictionCfg() *config.Cache {
+	c := Cfg()
+	c.Eviction = &config.EvictionCfg{
+		LRUMode:              config.LRUModeListing,
+		SoftLimitCoefficient: 0.8,              // Soft eviction will start on 8Mb
+		SoftMemoryLimitBytes: 1024 * 1024 * 10, // 10Mb
+		CallsPerSec:          5,
+		BackoffSpinsPerCall:  1024,
+	}
+	c.AdjustConfig()
+	return c
+}
