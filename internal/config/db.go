56e957887d99b2768fb723863e12d33f51d97228
Boris <glazunov2142@icloud.com>
2025-12-28 10:54:39 +0400

    feat: cfg.

--- internal/config/db.go
+++ internal/config/db.go
@@ -6,4 +6,5 @@ type DBCfg struct {
 	SizeBytes              int64         `yaml:"size"`
 	IsTelemetryLogsEnabled bool          `yaml:"stat_logs_enabled"`
 	TelemetryLogsInterval  time.Duration `yaml:"5s"`
+	CacheTimeEnabled       bool          `yaml:"cache_time_enabled"`
 }
